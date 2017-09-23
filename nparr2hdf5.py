02882bf28dbcca0e88767ab8a6f6170e9d2eff36
Yedarm Seong <mybirth0407@gmail.com>
2017-09-24 03:54:42 +0900

    variable name change and print type, shape

--- nparr2hdf5.py
+++ nparr2hdf5.py
@@ -2,14 +2,16 @@ import numpy as np
 import h5py
 
 # write
-write_arr = np.array([1,2,3,4,5,6,7,8])
+a = np.array([1,2,3,4,5,6,7,8])
 h5f = h5py.File('test.h5', 'w')
-h5f.create_dataset('arr', data=write_arr)
+h5f.create_dataset('arr', data=a)
 h5f.close()
 
 # read
 h5f = h5py.File('test.h5', 'r')
-read_arr = h5f['arr'][:]
+b = h5f['arr'][:]
 h5f.close()
 
 print(b)
+print(type(b))
+print(b.shape)
